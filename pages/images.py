0985dac3a55b64821a7419fca13ce30a2e26a23f
AdeyemiTestimony <90443750+AdeyemiTestimony@users.noreply.github.com>
2024-02-09 19:02:18 +0200

    Update images.py

--- pages/images.py
+++ pages/images.py
@@ -23,7 +23,8 @@ UNSPLASH_ACCESS_KEY = "MFcvfNHZ-u_kRAhK4oHT2uIIYtWVTxI-pk8qbr4sIR8"
 #     return [photo['urls']['regular'] for photo in data['results']], [photo.get('description') for photo in data['results']]
 
 def fetch_images(query, count=10):
-    per_page = max(count, 30)  # Fetch at least 30 images to ensure we have enough
+    #per_page = max(count, 30)
+    per_page = 30  # Fetch at least 30 images to ensure we have enough
     url = f"https://api.unsplash.com/search/photos/?query={query}&client_id={UNSPLASH_ACCESS_KEY}&per_page={per_page}"
     response = requests.get(url)
     data = response.json()
